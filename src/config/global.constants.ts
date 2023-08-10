8000fe7a1ccdeac1480249bae08b7a7e2b753db9
Ayvaz Miftakhov <ayvaz.miftakhov@gmail.com>
2023-08-10 13:27:14 +0300

    config value TOTAL_RAM_AVAILABLE updated

--- src/config/global.constants.ts
+++ src/config/global.constants.ts
@@ -1,5 +1,5 @@
 // TODO: get values from .env
-export const TOTAL_RAM_AVAILABLE = 15 * 1024 * 1024;
+export const TOTAL_RAM_AVAILABLE = 500 * 1024 * 1024;
 export const READ_FILE_RAM_RATIO = 0.5; // part of the RAM will go to the program operations
 export const INPUT_FILE_PATH = './test-data/input.txt';
 export const OUTPUT_FILE_PATH = './test-data/sorted-output.txt';
